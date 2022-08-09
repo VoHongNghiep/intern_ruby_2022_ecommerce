a49dbd4abdfcdbbe37264b5f0ddbf5db136a8dd0
r3tnuHq3IhgN <nghiephunter.dev.uet@gmail.com>
2022-08-09 09:26:25 +0700

    main_pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("jquery")
+require("js/bootstrap")
+require("js/jquery.easing.min")
+require("js/jquery.flexslider")
+require("js/jquery.waypoints.min")
+require("js/easy-responsive-tabs")
+require("js/minicart.min")
+require("js/move-top")
+require("js/scroll")
+require("js/responsive-tab")
+require("js/run-slider")
+require("js/responsiveslides.min")
 
 Rails.start()
 Turbolinks.start()
